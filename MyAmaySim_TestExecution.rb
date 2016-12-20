c193ba9f57e6b93f4466c40c307ae37be3fe385c
miltonjohnp <miltonjohnp@yahoo.com>
2016-12-21 04:34:02 +0800

    Update MyAmaySim_TestExecution.rb

--- MyAmaySim_TestExecution.rb
+++ MyAmaySim_TestExecution.rb
@@ -2,9 +2,9 @@ require_relative 'MyAmaySim_TestCases.rb'
 
 job = TestCases.new()
 
-#job.TestCase1
-#job.TestCase2
-#job.TestCase3
-#job.TestCase4
-#job.TestCase5
+job.TestCase1
+job.TestCase2
+job.TestCase3
+job.TestCase4
+job.TestCase5
 job.TestCase6
